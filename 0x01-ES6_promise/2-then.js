bf40771b2c1e6ef343daf9f2bd3a944d32147e2f
GKiarie <igitaukiarie@gmail.com>
2023-06-28 11:44:52 +0300

    promise with tree handlers

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,16 +1,8 @@
 export default function handleResponseFromAPI(promise) {
   promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
     .finally(() => {
       console.log('Got a response from the API');
-    })
-    .then(() => {
-      const response = {
-        status: 200,
-        body: 'success',
-      };
-      return response;
-    })
-    .catch(() => {
-        return new Error()
     });
 }
